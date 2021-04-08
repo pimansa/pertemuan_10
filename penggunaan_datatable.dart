e1eb6db5c3e1c32a6ac6af173bc7ce092d83a3f4
pimansa <78518329+pimansa@users.noreply.github.com>
2021-04-08 09:48:39 +0700

    Add files via upload

---
+++ penggunaan_datatable.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: MyApp()));
+}
+
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Rico Imanta Ginting - Kelas'),
+      ),
+      body: ListView(children: <Widget>[
+        Center(
+            child: Text('Data Mahasiswa',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))),
+        DataTable(
+          columns: [
+            DataColumn(label: Text('NIRM')),
+            DataColumn(label: Text('Nama Mahasiswa')),
+            DataColumn(label: Text('Jurusan')),
+          ],
+          rows: [
+            DataRow(cells: [
+              DataCell(Text('2018020375')),
+              DataCell(Text('Pitri Manda Sari')),
+              DataCell(Text('Sistem Informasi')),
+            ]),
+            DataRow(cells: [
+              DataCell(Text('2018020019')),
+              DataCell(Text('Gita')),
+              DataCell(Text('Hukum')),
+            ]),
+            DataRow(cells: [
+              DataCell(Text('2018021014')),
+              DataCell(Text('Putra Negara')),
+              DataCell(Text('Ekonomi')),
+            ]),
+          ],
+        ),
+      ]),
+    );
+  }
+}
